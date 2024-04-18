8153e316c25b6bcbc26bc5559a90c37dd9676390
prashanth103 <cheeralaprashanth10@gmail.com>
2024-04-18 05:11:31 +0000

    Code giving error

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+const path = require('path')
+
+const app = express()
+
+const dbPath = path.join(__dirname, 'goodreads.db')
+
+let db = null
+
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    })
+    app.listen(3000, () => {
+      console.log('Server Running at http://localhost:3000/')
+    })
+  } catch (error) {
+    console.log(`Db Error: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+initializeDbAndServer()
+
+app.get('/books/', async (request, response) => {
+  const getBooksQuery = `
+    SELECT *
+    FROM book
+    ORDER BY book_id;
+    `
+  const booksArray = await db.all(getBooksQuery)
+  response.send(booksArray)
+})
